30d20fb519154412348582059608952f70e168c0
Mia <miaagraahaam@gmail.com>
2025-12-10 22:33:22 -0600

    Add files via upload

---
+++ TodoRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoPOCO
+{
+    internal class TodoRepository
+    {
+        private List<TodoItem> _items = new List<TodoItem>();
+
+        public void Add(TodoItem item)
+        {
+            // TODO:  Add the item to the list
+        }
+
+        public TodoItem? Get(int id)
+        {
+            // TODO: Find and return the item with this ID
+            return null;
+        }
+
+        public List<TodoItem> GetAll()
+        {
+            // TODO:  Return every item in the list
+            return new List<TodoItem>();
+        }
+
+        public void Update(TodoItem item)
+        {
+            // TODO: Update the matching item in the list
+        }
+
+        public void Delete(int id)
+        {
+            // TODO: Remove the item with this ID
+        }
+    }
+    
+}
